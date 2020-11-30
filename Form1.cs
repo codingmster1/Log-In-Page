bffb21aad5a0e632ed76305d978418bc952dfe81
Jeff Doyle <doylejeff98@gmail.com>
2020-11-30 17:26:15 -0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LOG_IN_form
+{
+    public partial class dataBank : Form
+    {
+        public dataBank()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "Admin" && textBox2.Text == "123456")
+            {
+                MessageBox.Show("Welcome! Log in successfull!");
+            }
+            else 
+            {
+                MessageBox.Show("Username or Password Invalid!");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            PanelRight.Left = 398;
+            PanelLeft.Left = 34;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            PanelRight.Left = 34;
+            PanelLeft.Left = 398;
+        }
+    }
+}
